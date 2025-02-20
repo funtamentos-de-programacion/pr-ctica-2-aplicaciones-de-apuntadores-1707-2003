4ddfb67b92a0719edae6dab71ec89684f2a43c7a
Cantero Martel Mariana Itzel <cantero.mart@gmail.com>
2025-02-20 10:54:55 -0600

    Archivos modificados durante la practica

--- ejercicio4.c
+++ ejercicio4.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int main() {
-  int arr[] = {1, 2, 3, 4, 5};
+  int arr[ ] = {1, 2, 3, 4, 5};
   int *ptr = arr;
   
   printf("Valores originales:\n");
   for (int i = 0; i < 5; i++) {
     //Escribe el codigo para imprimir los valores usado el apuntador
+   printf("%d ", (*ptr+1));
   }
-  
-  for (int i = 0; i < 5; i++) {
-    // Duplicar cada elemento
-  }
-  
+ printf("\n");
+
+ for(int i = 0;1 < 5; i++){
+int *(ptr+1)*= 2; //*(ptr+i)= *(ptr+1)*2
+ }
   printf("\nValores modificados:\n");
   for (int i = 0; i < 5; i++) {
     //Escribe el codigo para imprimir los valores usado el apuntador
+    printf("%d ", *(ptr+i));
   }
+printf("\n");
   return 0;
 }
